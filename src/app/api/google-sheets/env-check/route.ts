a76e0815aa9f8f636e96bf4e4c5b920d39894a73
softlandksa <operation@softland.me>
2026-05-02 14:24:48 +0300

    debug: add google sheets env check endpoint

---
+++ src/app/api/google-sheets/env-check/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+  return Response.json({
+    hasClientEmail: !!process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    hasPrivateKey: !!process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+    hasSpreadsheetId: !!process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
+    hasRange: !!process.env.GOOGLE_SHEETS_RANGE,
+    hasSyncSecret: !!process.env.GOOGLE_SHEETS_SYNC_SECRET,
+  });
+}
